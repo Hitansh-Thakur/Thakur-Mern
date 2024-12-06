f727bea0dab77b540c72240abe35d64c64ac91bf
Hitansh Thakur <thakurhitansh4325@gmail.com>
2024-12-06 13:14:57 +0530

    style:style navbar and added images to the project

--- thakur-frontend/src/Components/Navbar.jsx
+++ thakur-frontend/src/Components/Navbar.jsx
@@ -1,14 +1,19 @@
-import React from 'react'
-import{ 
+import { React, Image } from 'react'
+import logo from '../assets/logo.png'
+
+import {
     NavLink
 } from 'react-router-dom'
 
 const Navbar = () => {
     return (
         <>
-            <div className="flex">
-                <ul>
-                    <li>
+            <div className="flex w-full items-center justify-between py-4 px-10 bg-orange-200">
+                <div className="">
+                    <img className='w-[10rem] ' src={logo}  alt="Thakur Industries" />
+                </div>
+                <ul className='flex gap-10 text-lg'>
+                    <li >
                         <NavLink to="/">Home</NavLink>
                     </li>
                     <li>
@@ -17,7 +22,6 @@ const Navbar = () => {
                     <li>
                         <NavLink to='/products'>Products</NavLink>
                     </li>
-                    
                 </ul>
             </div>
         </>
